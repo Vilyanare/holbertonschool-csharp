2a91eb9f2ff3164929af2022ef2d26db1c61d570
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2018-09-05 20:53:39 -0700

    Removed a write statement

--- 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -4,14 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        string space = ", ";
         for (int i = 0; i < 100; i++)
         {
             Console.Write("{0:D2}", i);
-            if (i != 99)
+            if (i == 99)
             {
-                Console.Write(", ");
+                space = "\n";
             }
+                Console.Write(space);
         }
-        Console.WriteLine();
     }
 }
