23256fb796db5de7d2b5ea9ed4543af8ef5092f0
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2018-09-18 22:37:25 -0700

    Added condition for if there was no node or it was the last node

--- 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -7,15 +7,24 @@ class LList
     {
         LinkedListNode<int> node = null;
         LinkedListNode<int> newNode = new LinkedListNode<int>(n);
+        int broke = 0;
         foreach (int nums in myLList)
         {
             if (n < nums)
             {
                 node = myLList.Find(nums);
+                broke = 1;
                 break;
             }
         }
-        myLList.AddBefore(node, newNode);
+        if (broke == 0)
+        {
+            myLList.AddLast(node);
+        }
+        if (node != null)
+        {
+            myLList.AddBefore(node, newNode);
+        }
         return newNode;
     }
 }
