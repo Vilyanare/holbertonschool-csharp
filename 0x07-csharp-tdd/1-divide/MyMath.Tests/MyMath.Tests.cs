54a810b1edb50b1b58f8b071f4c23d8909e0f005
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2018-09-25 16:40:39 -0700

    First commit

---
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace Tests
+{
+    public class Tests
+    {
+        private StringWriter sw = new StringWriter();
+        private TextWriter originalOutput = Console.Out;
+        [Test]
+        public void positiveDefault()
+        {
+            int [,] matrix = new int[,] { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+            int [,] result = MyMath.Matrix.Divide(matrix, 2);
+            int [,] end = new int [,] { {0, 1, 1,}, {2, 2, 3}, {3, 4, 4} };
+            Assert.AreEqual(end, result);
+        }
+        [Test]
+        public void ifDivIs0()
+        {
+            Console.SetOut(sw);
+            int [,] matrix = new int[,] { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+            int [,] result = MyMath.Matrix.Divide(matrix, 0);
+            Assert.AreEqual("Num cannot be 0\n", sw.ToString());
+            Console.SetOut(originalOutput);
+        }
+        [Test]
+        public void isMatrixNull()
+        {
+            int [,] result = MyMath.Matrix.Divide(null, 2);
+            Assert.AreEqual(null, result);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>
+    /// Provides method to divide all integers in a matrix by given integer.
+    /// </summary>
+    public class Matrix
+    {
+        /// <summary>
+        /// Returns given matrix with all integers divided by given integer.
+        /// </summary>
+        /// <param name="matrix">Matrix to divide.</param>
+        /// <param name="num">Number to divide by.</param>
+        /// <returns>Matrix with quotients or null if error</returns>
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            if (num == 0)
+            {
+                Console.WriteLine("Num cannot be 0");
+                return null;
+            }
+            if (matrix == null)
+            {
+                return null;
+            }
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = matrix[i, j] / num;
+                }
+            }
+            return matrix;
+        }
+    }
+}
