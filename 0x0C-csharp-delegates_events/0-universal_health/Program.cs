4c386d2f5f3bd3390659f39e20a6eab7a9bc6e5b
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2019-02-09 17:54:46 -0800

    First commit

---
+++ 0x0C-csharp-delegates_events/0-universal_health/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Player
+{
+    private string name;
+    private float maxHP;
+    private float hp;
+
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHP > 0)
+            this.maxHP = maxHP;
+        else
+            this.maxHP = 100f;
+        this.hp = this.maxHP;
+    }
+}
---
+++ 0x0C-csharp-delegates_events/1-damage_delegation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Player
+{
+    private string name;
+    private float maxHP;
+    private float hp;
+
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHP > 0)
+            this.maxHP = maxHP;
+        else
+            this.maxHP = 100f;
+        this.hp = this.maxHP;
+    }
+}
+
+public delegate void CaluclateHealth(float amount);
+
+public void TakeDamage(float damage)
+{
+    System.Console.WriteLine($"{name} takes damage!");
+    if (damage < 0)
+        damage = 0;
+}
+public void HealDamage(float heal)
+{
+    System.Console.WriteLine($"{name} heals {heal} HP!");
+    if (heal < 0)
+        heal = 0;
+}
