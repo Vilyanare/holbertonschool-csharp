5b6830b8ed3308ff0a92ec9d2cb53c5c7611b329
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2019-02-09 18:21:00 -0800

    Added period to end of error message

--- 0x0C-csharp-delegates_events/0-universal_health/Program.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/Program.cs
@@ -15,7 +15,7 @@ class Player
         }
         else
         {
-            System.Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default");
+            System.Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
             this.maxHp = 100f;
         }
         this.hp = this.maxHp;
